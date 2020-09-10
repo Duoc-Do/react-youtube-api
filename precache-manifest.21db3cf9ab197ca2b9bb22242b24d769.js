856c855d8de66811cb6e630451cf2c6cc49cd337
DuocDo <duocdo.it@gmail.com>
2020-09-10 19:48:38 +0700

    Updates

---
+++ precache-manifest.21db3cf9ab197ca2b9bb22242b24d769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769be07ecec03c19f6ec7ed6892317ce",
+    "url": "/react-youtube-api/index.html"
+  },
+  {
+    "revision": "60f33343fda8e9298d18",
+    "url": "/react-youtube-api/static/js/2.34562175.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-youtube-api/static/js/2.34562175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462cc9a6a728c3fbaa69",
+    "url": "/react-youtube-api/static/js/main.98f609b0.chunk.js"
+  },
+  {
+    "revision": "098769e810cf3814f2b6",
+    "url": "/react-youtube-api/static/js/runtime-main.e48d3e84.js"
+  }
+]);
